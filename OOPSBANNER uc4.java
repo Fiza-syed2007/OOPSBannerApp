10f0610893aaa3903f9da2f105418c443e85df10
fiza-syed2007 <fs8451@srmist.edu.in>
2026-03-02 13:41:13 +0530

    FIZA ADDED UC4

---
+++ OOPSBANNER uc4.java
@@ -0,0 +1,44 @@
+public class OOPSBannerUC3 {
+
+    public static void main(String[] args) {
+
+        String star = "*";
+        String space = " ";
+
+        String line1 = String.join("", 
+                star, star, star, star, star, space,
+                star, star, star, star, star, space,
+                star, star, star, star, star, space,
+                star, star, star, star, star);
+
+        String line2 = String.join("", 
+                star, space, space, space, star, space,
+                star, space, space, space, star, space,
+                star, space, space, space, star, space,
+                star);
+
+        String line3 = String.join("", 
+                star, space, space, space, star, space,
+                star, star, star, star, star, space,
+                star, star, star, star, star, space,
+                star, star, star, star);
+
+        String line4 = String.join("", 
+                star, space, space, space, star, space,
+                star, space, space, space, star, space,
+                star, space, space, space, space, space,
+                space, space, space, star);
+
+        String line5 = String.join("", 
+                star, star, star, star, star, space,
+                star, star, star, star, star, space,
+                star, space, space, space, space, space,
+                star, star, star, star);
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+    }
+}
